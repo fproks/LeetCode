8573bd11ee37f7864de7261c014ade2b6dc114ed
ezlinho <hongshuai.lin@ericsson.com>
2019-07-05 08:45:53 +0800

    951. Flip Equivalent Binary Trees

--- pythonCode/Solution/MediumSoultion/ListSolution.py
+++ pythonCode/Solution/MediumSoultion/ListSolution.py
@@ -80,6 +80,15 @@ class ListSolution:
         res += self.distributeCoins(root.right)
         return res
 
+    # 951. Flip Equivalent Binary Trees
+    # https://leetcode.com/problems/flip-equivalent-binary-trees/
+    def flipEquiv(self, root1: TreeNode, root2: TreeNode) -> bool:
+        def fanz(root1: TreeNode, root2: TreeNode):
+            if not root1 and not root2: return True
+            if root1 and root2 and root1.val == root2.val:
+                return (fanz(root1.left, root2.right) and fanz(root1.right, root2.left)) or (
+                        fanz(root1.left, root2.left) and fanz(root1.right, root2.right))
+            else:
+                return False
 
-
-
+        return fanz(root1, root2)
