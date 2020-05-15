dcd06836024c817eb6235ea5854af539e0dd8afb
Linhos <linhos@126.com>
2020-05-15 13:40:15 +0800

    560. Subarray Sum Equals K

--- LeetCode/src/main/java/LeetCode/medium/ArraysSolution3.java
+++ LeetCode/src/main/java/LeetCode/medium/ArraysSolution3.java
@@ -478,5 +478,19 @@ public class ArraysSolution3 {
         return  cou;
     }
 
+    //560. Subarray Sum Equals K
+    public int subarraySum(int[] nums, int k) {
+       HashMap<Integer,Integer> map =new HashMap<>();
+       map.put(0,1);
+       int sum=0;
+       int count=0;
+       for (int n:nums){
+           sum+=n;
+           count+=map.getOrDefault(sum-k,0);
+           map.put(sum,map.getOrDefault(sum,0)+1);
+       }
+       return  count;
+    }
+
 
 }
