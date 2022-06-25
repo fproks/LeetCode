e736a8f825ead1283aecf2ed0c73ae9e0f3e267b
fproks <fproks@126.com>
2022-06-25 18:16:20 +0800

    剑指 Offer II 091. 粉刷房子

--- LeetCode/src/main/java/LeetCode/medium/Kotlin/ArraysSolution.kt
+++ LeetCode/src/main/java/LeetCode/medium/Kotlin/ArraysSolution.kt
@@ -4,6 +4,7 @@ import LeetCode.struct.TreeNode
 import java.util.*
 import kotlin.collections.ArrayDeque
 import kotlin.collections.ArrayList
+import kotlin.math.min
 
 class ArraysSolution {
 
@@ -67,133 +68,145 @@ class ArraysSolution {
     }
 
     fun maxRotateFunction(nums: IntArray): Int {
-        val sum=nums.sum()
-        var f0=0
-        for ( i in nums.indices){
-            f0+=nums[i]*i
+        val sum = nums.sum()
+        var f0 = 0
+        for (i in nums.indices) {
+            f0 += nums[i] * i
         }
-        var res =f0
+        var res = f0
 
-        for (i in 1 until nums.size){
-            f0=f0+sum-nums.size*nums[nums.size-i]
-            if (f0>res)res=f0
+        for (i in 1 until nums.size) {
+            f0 = f0 + sum - nums.size * nums[nums.size - i]
+            if (f0 > res) res = f0
         }
-        return  res
+        return res
     }
 
     //436. 寻找右区间
     fun findRightInterval(intervals: Array<IntArray>): IntArray {
-        val startArr=intervals.mapIndexed { index, ints -> intArrayOf(ints[0],index)  }.sortedBy { it[0] }
-        val resutl=kotlin.collections.ArrayList<Int>()
-        for(i in intervals){
-            val arr=startArr.find { ints: IntArray ->ints[0]>=i[1]  }
-            if(arr!==null)resutl.add(arr[1])
+        val startArr = intervals.mapIndexed { index, ints -> intArrayOf(ints[0], index) }.sortedBy { it[0] }
+        val resutl = kotlin.collections.ArrayList<Int>()
+        for (i in intervals) {
+            val arr = startArr.find { ints: IntArray -> ints[0] >= i[1] }
+            if (arr !== null) resutl.add(arr[1])
             else resutl.add(-1)
         }
-        return  resutl.toIntArray()
+        return resutl.toIntArray()
     }
 }
 
-class  LexicalOrderSolution {
+class LexicalOrderSolution {
     fun lexicalOrder(n: Int): List<Int> {
-        val list =kotlin.collections.ArrayList<Int>()
-        for ( i in 1 until 10){
-            dfs(n,i,list)
+        val list = kotlin.collections.ArrayList<Int>()
+        for (i in 1 until 10) {
+            dfs(n, i, list)
         }
-        return  list
+        return list
     }
 
-    fun  dfs(n:Int,target:Int,list : ArrayList<Int>){
-        if (target>n)return
+    fun dfs(n: Int, target: Int, list: ArrayList<Int>) {
+        if (target > n) return
         list.add(target)
-        val tmp=target*10
-        for (i in 0 until 10){
-            dfs(n,tmp+i,list)
+        val tmp = target * 10
+        for (i in 0 until 10) {
+            dfs(n, tmp + i, list)
         }
     }
 }
 
 class PickSolution(val nums: IntArray) {
     fun pick(target: Int): Int {
-        val count=nums.count { it==target }
-        val r =Random().nextInt(count)
-        var idx=0
-        for ( i in nums.indices){
-            if (nums[i]==target){
-                if(idx==r)return  i
+        val count = nums.count { it == target }
+        val r = Random().nextInt(count)
+        var idx = 0
+        for (i in nums.indices) {
+            if (nums[i] == target) {
+                if (idx == r) return i
                 else idx++
             }
         }
-        return  -1
+        return -1
     }
 
 
     fun pacificAtlantic(heights: Array<IntArray>): List<List<Int>> {
-        val list =kotlin.collections.ArrayList<List<Int>>()
-        val m =heights.size
-        if(m<1)return list
-        val n =heights[0].size
-        val pacific = Array(m){BooleanArray(n){false} }
-        val atlantic =Array(m){BooleanArray(n){false} }
-        for ( i in heights.indices){
-            pacificAtlanticDfs(heights,i,0,pacific,heights[i][0])
-            pacificAtlanticDfs(heights,i,n-1,atlantic,heights[i][n-1])
+        val list = kotlin.collections.ArrayList<List<Int>>()
+        val m = heights.size
+        if (m < 1) return list
+        val n = heights[0].size
+        val pacific = Array(m) { BooleanArray(n) { false } }
+        val atlantic = Array(m) { BooleanArray(n) { false } }
+        for (i in heights.indices) {
+            pacificAtlanticDfs(heights, i, 0, pacific, heights[i][0])
+            pacificAtlanticDfs(heights, i, n - 1, atlantic, heights[i][n - 1])
         }
-        for (i in 0 until n){
-            pacificAtlanticDfs(heights,0,i,pacific,heights[0][i])
-            pacificAtlanticDfs(heights,m-1,i,atlantic,heights[m-1][i])
+        for (i in 0 until n) {
+            pacificAtlanticDfs(heights, 0, i, pacific, heights[0][i])
+            pacificAtlanticDfs(heights, m - 1, i, atlantic, heights[m - 1][i])
         }
 
-        for (i in 0 until m){
-            for (j in 0 until n){
-                if(pacific[i][j] && atlantic[i][j])list.add(listOf(i,j))
+        for (i in 0 until m) {
+            for (j in 0 until n) {
+                if (pacific[i][j] && atlantic[i][j]) list.add(listOf(i, j))
             }
         }
-        return  list
+        return list
 
     }
-    private fun pacificAtlanticDfs(heights: Array<IntArray>, x :Int, y:Int, visited:Array<BooleanArray>, pre: Int){
-        if (x<0 || y<0 || x>=heights.size || y>=heights[0].size || visited[x][y] || heights[x][y] <pre)
+
+    private fun pacificAtlanticDfs(heights: Array<IntArray>, x: Int, y: Int, visited: Array<BooleanArray>, pre: Int) {
+        if (x < 0 || y < 0 || x >= heights.size || y >= heights[0].size || visited[x][y] || heights[x][y] < pre)
             return
-        visited[x][y]=true
-        pacificAtlanticDfs(heights, x+1, y, visited, pre)
-        pacificAtlanticDfs(heights, x-1, y, visited, pre)
-        pacificAtlanticDfs(heights, x, y+1, visited, pre)
-        pacificAtlanticDfs(heights, x, y-1, visited, pre)
+        visited[x][y] = true
+        pacificAtlanticDfs(heights, x + 1, y, visited, pre)
+        pacificAtlanticDfs(heights, x - 1, y, visited, pre)
+        pacificAtlanticDfs(heights, x, y + 1, visited, pre)
+        pacificAtlanticDfs(heights, x, y - 1, visited, pre)
     }
 
     fun numSubarrayProductLessThanK(nums: IntArray, k: Int): Int {
-        if(k==0 || k==1)return  0
-        var l=0
-        var prod=1
-        var res =0
-        for (i in nums.indices){
-            prod*=nums[i]
-            while (prod>=k){
-                prod /=nums[l++]
+        if (k == 0 || k == 1) return 0
+        var l = 0
+        var prod = 1
+        var res = 0
+        for (i in nums.indices) {
+            prod *= nums[i]
+            while (prod >= k) {
+                prod /= nums[l++]
             }
-            res+=i-l+1
+            res += i - l + 1
         }
-        return  res
+        return res
     }
 
     fun largestValues(root: TreeNode?): List<Int> {
-        val res =kotlin.collections.ArrayList<Int>()
-        if(root==null)return res
+        val res = kotlin.collections.ArrayList<Int>()
+        if (root == null) return res
         val queue = LinkedList<TreeNode>()
         queue.add(root)
-        while (!queue.isEmpty()){
-            var max=queue.peek().`val`
-            val tmp=queue.toList()
+        while (!queue.isEmpty()) {
+            var max = queue.peek().`val`
+            val tmp = queue.toList()
             queue.clear()
-            for (i in tmp){
-                if (i.`val`>max)max=i.`val`
-                if(i.left!=null)queue.add(i.left)
-                if(i.right!=null)queue.add(i.right)
+            for (i in tmp) {
+                if (i.`val` > max) max = i.`val`
+                if (i.left != null) queue.add(i.left)
+                if (i.right != null) queue.add(i.right)
             }
             res.add(max)
         }
         return res
+    }
+
+    fun minCost(costs: Array<IntArray>): Int {
+        val dp = Array(size = 100) { IntArray(3) { 0 } }
+        dp[0] = costs[0]
+        for (i in 1 until  costs.size){
+            dp[i][0]= min( dp[i-1][2],dp[i-1][1])+costs[i][0]
+            dp[i][1]= min( dp[i-1][0],dp[i-1][2])+costs[i][1]
+            dp[i][2]= min( dp[i-1][0],dp[i-1][1])+costs[i][2]
+        }
+        return min( min(dp[costs.size-1][0],dp[costs.size-1][1]),dp[costs.size-1][2])
     }
 
 }
