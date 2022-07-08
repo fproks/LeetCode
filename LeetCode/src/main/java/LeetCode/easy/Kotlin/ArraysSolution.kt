8d1d9a530f75fa8383313ab8434fb240626d6322
fproks <fproks@126.com>
2022-07-08 14:32:53 +0800

    1217. 玩筹码

--- LeetCode/src/main/java/LeetCode/easy/Kotlin/ArraysSolution.kt
+++ LeetCode/src/main/java/LeetCode/easy/Kotlin/ArraysSolution.kt
@@ -268,5 +268,13 @@ class ArraysSolution {
         }
     }
 
-
+    fun minCostToMoveChips(position: IntArray): Int {
+        var odd = 0
+        var even = 0
+        for (i in position) {
+            if (i % 2 == 0) even++
+            else odd++
+        }
+        return if (odd < even) odd else even
+    }
 }
