6302a19f82501d52556d2f30ae90ad6cf45bd8e9
fprok <fproks@126.com>
2022-08-23 20:54:13 +0800

    cifar with keras and pytorch

--- deepLearning/learning/pytorchExt.py
+++ deepLearning/learning/pytorchExt.py
@@ -30,7 +30,23 @@ class PytorchExt(object):
         self.test_data = FloatTensor(dataReader.XTest)
         self.test_label = FloatTensor(dataReader.YTest)
 
-    def iterTrain(self, title):
+    def generateDataFromNumpy(self, train_x, train_y, test_x, test_y, val_x=None, val_y=None):
+        self.dataLoader = DataLoader(
+            dataset=TensorDataset(FloatTensor(train_x), FloatTensor(train_y)),
+            batch_size=self.batch_size, shuffle=True)
+        self.test_data = FloatTensor(test_x)
+        self.test_label = FloatTensor(test_y)
+        if (val_x is not None) and (val_y is not None):
+            self.val_data = FloatTensor(val_x)
+            self.val_label = FloatTensor(val_y)
+
+    def iterTrain(self, need_check=False, CUDA=False):
+        if CUDA is True:
+            self.test_data = self.test_data.cuda()
+            self.test_label = self.test_label.cuda()
+            if (self.val_data is not None) and (self.val_label is not None):
+                self.val_data = self.val_data.cuda()
+                self.val_label = self.val_label.cuda()
         train_loss = []
         train_acc = []
         test_loss = []
@@ -39,18 +55,29 @@ class PytorchExt(object):
             epoch_loss = []
             epoch_acc = []
             for step, (batch_x, batch_y) in enumerate(self.dataLoader):
+                batch_len = len(batch_y)
+                if CUDA:
+                    batch_x = batch_x.cuda()
+                    batch_y = batch_y.cuda()
                 pred = self.model(batch_x)
                 loss = self.loss_func(pred, batch_y)
+                acc = torch.eq(pred.argmax(1), batch_y.argmax(1)).sum() / batch_len
                 self.optimizer.zero_grad()
                 loss.backward()
                 self.optimizer.step()
-                if (step + 1) % self.checkpoint == 0:
-                    pred = self.model(self.val_data)
-                    loss = self.loss_func(pred, self.val_label)
-                    acc = torch.eq(pred.argmax(1), self.val_label.argmax(1)).sum() / len(self.val_label)
-                    epoch_acc.append(acc.cpu().data)
-                    epoch_loss.append(loss.cpu().data)
+                print(f"\r epoch {epoch}--step {step}: loss {loss.cpu().data:.6f},acc= {acc * 100:.6f}%", end="",
+                      flush=True)
+                if need_check and (step + 1) % self.checkpoint == 0:
+                    val_data = self.val_data if self.val_data is not None else self.test_data
+                    val_label = self.val_label if self.val_label is not None else self.test_label
+                    pred = self.model(val_data)
+                    loss = self.loss_func(pred, val_label)
+                    acc = torch.eq(pred.argmax(1), val_label.argmax(1)).sum() / len(self.val_label)
+                    print()
                     print(f"epoch {epoch}--step {step}: loss={loss.cpu().data:.6f}, acc={acc * 100:.6f}%")
+                epoch_acc.append(acc.cpu().data)
+                epoch_loss.append(loss.cpu().data)
+            print()
             train_loss.append(np.mean(epoch_loss))
             train_acc.append(np.mean(epoch_acc))
             pred = self.model(self.test_data)
@@ -61,13 +88,13 @@ class PytorchExt(object):
             print(
                 f"\033[0;35mepoch {epoch}: train_loss: {np.mean(epoch_loss):.6f}, test_loss: {loss.cpu().data:.6f}, "
                 f"train_acc={np.mean(epoch_acc) * 100:.6f}%, test_acc={acc * 100:.6f}%\033[0m")
-        self.pltshow(f"LOSS: {title}", train_loss, test_loss)
-        self.pltshow(f"ACC: {title}", train_acc, test_acc)
+        return {"train_accuracy": train_acc, "test_accuracy": test_acc, "train_loss": train_loss,
+                "test_loss": test_loss}
 
     @staticmethod
-    def pltshow( title, loss_train, loss_test):
+    def pltshow(title, loss_train, loss_test, legend1, legend2):
         plt.plot([i for i in range(len(loss_train))], loss_train)
         plt.plot([i for i in range(len(loss_test))], loss_test)
         plt.title(title)
-        plt.legend(["Val", "Test"])
+        plt.legend([legend1, legend2])
         plt.show()
